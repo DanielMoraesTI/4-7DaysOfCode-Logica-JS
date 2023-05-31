16e8d37a1e98921668a5292ef96b4f051228e11a
Daniel Moraes <124385951+DanielMoraesTI@users.noreply.github.com>
2023-05-31 12:54:28 +0100

    JS com os 2 exercícicios do desafio do dia 04 de 7DaysOfCode

--- app.js
+++ app.js
@@ -1,51 +1,39 @@
+//Desafio dia 4 com número fixo
 let numeroSecreto = 7;
-let quantidadeTentativas = 1;
 let nome = prompt('Qual seu nome?');
-let palpite = prompt(`${nome}, digite um número entre 1 e 10`);
-    if (palpite == numeroSecreto) {
+let palpite = "";
+let acertou = false;
+
+for(let i = 0; i < 3; i++) {
+  palpite = prompt(`${nome}, digite um número entre 1 e 10, você tem 03 tentativas para acertar!`);
+  if (palpite == numeroSecreto) {
     alert(`Certa resposta ${nome}, agora pode jogar na loteria!`);
+    acertou = true;
+    break;
 }
-while(palpite != numeroSecreto) {
-
-    if (palpite > numeroSecreto) {
-        alert(`Errou... o número secreto é menor que ${palpite}, tente novamente!`);
-        let palpite = prompt(`${nome}, digite um número entre 1 e 10`);
-    }
-    if (palpite < numeroSecreto) {
-        alert(`Errou..., o número secreto é maior que ${palpite}, tente novamente!`);
-        let palpite = prompt(`${nome}, digite um número entre 1 e 10`);
-    }
-    alert('Quantidade de tentativas: ' + quantidadeTentativas++)
+alert("Resposta errada!");
 }
+if(!acertou) {
+  alert(`Infelizmente, você não acertou ${nome}. O número era ${numeroSecreto}!`);
+}
+   
 
-let numeroSecreto = 7;
-let quantidadeTentativas = 1;
+
+//Desafio dia 4 com número aleatório
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
 let nome = prompt('Qual seu nome?');
-let palpite = prompt(`${nome}, digite um número entre 1 e 10`);
-    if (palpite == numeroSecreto) {
-    alert(`Certa resposta ${nome}, agora pode jogar na loteria!`);
-}
-    if (palpite > numeroSecreto) {
-        alert(`Errou... o número secreto é menor que ${palpite}, tente novamente!`);
-        let palpite = prompt(`${nome}, digite um número entre 1 e 10`);
-    }
-    if (palpite < numeroSecreto) {
-        alert(`Errou..., o número secreto é maior que ${palpite}, tente novamente!`);
-        let palpite = prompt(`${nome}, digite um número entre 1 e 10`);
-    }
-    alert('Quantidade de tentativas: ' + quantidadeTentativas++)
+let palpite = "";
+let acertou = false;
 
-var numeroSecreto = parseInt(Math.random() * 1001 + 1)
-var quantidadeTentativas = 1
-var jogador = prompt('Qual é o seu nome?')
-while(chute != numeroSecreto) {
-  var chute = prompt('Digite um número entre 1 e 1000')
-  if (chute == numeroSecreto) {
-    alert('Certa resposta ' + jogador + ', agora pode tentar a sorte na Mega Sena! Volte sempre e me deixem críticas contrutivas, obrigado!')
-  } else if (chute > numeroSecreto) {
-    alert('Errou... o número secreto é menor do que ' + chute + ', tente novamente!')
-  } else if (chute < numeroSecreto) {
-    alert('Errou... o número secreto é maior do que ' + chute + ', tente novamente!')
-  }
-  alert('Quantidade de tentaivas: '+ quantidadeTentativas++)
+for(let i = 0; i < 3; i++) {
+  palpite = prompt(`${nome}, digite um número entre 1 e 10, você tem 03 tentativas para acertar!`);
+  if (palpite == numeroSecreto) {
+    alert(`Certa resposta ${nome}, agora pode jogar na loteria!`);
+    acertou = true;
+    break;
+}
+alert("Resposta errada!");
+}
+if(!acertou) {
+  alert(`Infelizmente, você não acertou ${nome}. O número era ${numeroSecreto}!`);
 }
